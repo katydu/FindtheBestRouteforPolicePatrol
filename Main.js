6147082c8078c4bc2bfa56b167c816d205e9f0ca
katy_du <surekaty0919@gmail.com>
2021-12-22 23:22:02 +0800

    first commit

---
+++ Main.js
@@ -0,0 +1,82 @@
+﻿var inputFiles = [];
+var inputFiles_split = [];
+var places = [];
+//FileInput
+var fileInput = document.getElementById("fileInput");
+var fileDisplayArea = document.getElementById("fileDisplayArea");
+// upload file function
+fileInput.addEventListener("change", function (e) {
+  //reader use to read the file
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    //display the content inside the file
+    fileDisplayArea.innerText = reader.result;
+    //clean the data
+    inputFiles = reader.result.split("\r\n");
+    inputFiles_split = [];
+    for (i = 1; i < inputFiles.length; i++) {
+      inputFiles_split.push(inputFiles[i].split(","));
+    }
+  };
+  //put csv file into file
+  var file = fileInput.files[0];
+
+  reader.readAsText(file);
+});
+
+//Google api
+var tw = { lat: 25.105497, lng: 121.597366 };
+var mapOptions = {
+  center: tw,
+  zoom: 7, //地圖的縮放倍率
+  mapTypeId: google.maps.MapTypeId.ROADMAP,
+};
+
+//create map
+var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+//create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+//create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+//bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
+
+function calcRoute() {
+  //split出places用在waypoints跟頭尾 2021/12/11 by lynnie hsiao
+  places = [];
+  for (i = 0; i < inputFiles_split.length; i++) {
+    if (inputFiles_split[i].length > 1) {
+      places.push({ location: inputFiles_split[i][2], stopover: true });
+    }
+  }
+
+  //create request
+  var request = {
+    //民權一派出所
+    origin: { lat: 25.060142842489007, lng: 121.52792382523405 }, //places[0].location, 第一點
+    destination: { lat: 25.060142842489007, lng: 121.52792382523405 }, //places[places.length - 1].location, 最後一點
+    // waypoints: [
+    //   { location: place1, stopover: true },
+    // ],
+    waypoints: places.slice(1, places.length - 1), //去掉第一和最後的
+    travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+    optimizeWaypoints: true,
+  };
+
+  //pass the request to the route method
+  directionsService.route(request, function (result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      //display route
+      directionsDisplay.setDirections(result);
+    } else {
+      //delete route from map
+      directionsDisplay.setDirections({ routes: [] });
+      //center map in Taiwan
+      map.setCenter(tw);
+      //show error message
+      output.innerHTML =
+        "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i>error</div>";
+    }
+  });
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Find the Best Route for Police's Patrol
+## 功能描述
+本計畫發現警察巡邏方式有一種為不定線巡邏，這種巡邏方式沒有固定路線，這有個缺點是效率不高，因此希望有個系統能根據犯罪相關地點數據來規劃一條最佳巡邏路線進而幫助警察可以最有效率的執勤。
+## 開發環境
+1. Google API
+   * [Direction API](https://icelandcheng.medium.com/%E4%BD%BF%E7%94%A8google-map-api-directions-service-%E7%8D%B2%E5%8F%96%E5%8F%8A%E9%A1%AF%E7%A4%BA%E6%9C%80%E4%BD%B3%E8%B7%AF%E5%BE%91-cf82060b90f2 "link")
+   * [Maps JavaScript API](https://medium.com/front-end-augustus-study-notes/google-map-api-1-a4e794b0162f "link")
+   * 補充:[Google API申請教學步驟](https://ithelp.ithome.com.tw/articles/10283037 "link")
+2. JavaScript
+3. HTML
+4. CSS
+## 專案結構簡介
+
+|  檔案/資料夾 | 說明 |
+| --------------- | --------------- |
+| Content | 擺放bootstrap要用到的檔案 |
+| fonts | 擺放用到的字型檔案 |
+| App.css |設計前端網頁的美化部分 |
+|Index.html|網站首頁|
+| Main.js | 上傳檔案功能+Google API運用 |
+| ant.js | 蟻群演算法的javascript版本 |
+## 作者列表
+柴柴+蕭嫻
